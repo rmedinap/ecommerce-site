253e69b30383a6e2e731608c42670f5bbd12de5d
JesusSayes <jesussayes@gmail.com>
2013-08-28 19:00:33 -0500

    Orden en Home

--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,3 +1,3 @@
-Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", :name => "pages_in_header",
-:insert_top => "#nav-bar",
-:partial => "spree/static_content/static_content_header", :disabled => false)
+#Deface::Override.new(:virtual_path => "spree/shared/_nav_bar", :name => "pages_in_header",
+#:insert_top => "#nav-bar",
+#:partial => "spree/static_content/static_content_header", :disabled => false)
