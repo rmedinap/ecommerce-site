1d8923910969b65367279ea3d763bb7ff55ab165
JesusSayes <jesussayes@gmail.com>
2013-08-28 19:21:45 -0500

    Quitando spree_contact_us y spree-easy-contact

--- config/initializers/spree_contact_us.rb
+++ config/initializers/spree_contact_us.rb
@@ -1,23 +1,24 @@
 # Use this initializer to configure the contact mailer.
+# ### Comentados...
 
-SpreeContactUs.setup do |config|
+### SpreeContactUs.setup do |config|
 
   # ==> Mailer Configuration
 
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = "contactenos@yupiyap.com"
+###  config.mailer_from = "contactenos@yupiyap.com"
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "contactenos@yupiyap.com"
+###  config.mailer_to = "contactenos@yupiyap.com"
 
   # ==> Form Configuration
 
   # Configure the form to ask for the users name.
-  config.require_name = true
+###  config.require_name = true
 
   # Configure the form to ask for a subject.
-  config.require_subject = true
+###  config.require_subject = true
 
-end
+###end
