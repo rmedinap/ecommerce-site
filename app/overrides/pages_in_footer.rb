80a98270955c855aca1ac2eec86ddddecfe156f9
Rafael Medina <rmedinap@gmail.com>
2013-10-01 20:54:11 -0500

    cambio dIcono de facebook, link al blog en menu principal y footer, eliminado el detalle por dategoria en homepage, eliminados precios en el homepage y productos destacados en 3 columnas

--- app/overrides/pages_in_footer.rb
+++ app/overrides/pages_in_footer.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/shared/_footer",
                      :name => "contact_in_footer",
                      :insert_bottom => "#footer-right #footer-pages ul",
-                     :text => "<li><a href='/contact'>Contact</a></li>",
+                     :text => "<li><a href='/blog'>Blog</a></li> | <li><a href='/contact'>Contacto</a></li>",
                      :disabled => false
                     )
--- app/overrides/pages_in_header.rb
+++ app/overrides/pages_in_header.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "spree/shared/_main_nav_bar",
                      :name => "contact_in_header",
                      :insert_bottom => "#main-nav-bar",
-                     :text => "<li><a href='/contact'>Contact</a></li>",
+                     :text => "<li><a href='/blog'>Blog</a></li> | <li><a href='/contact'>Contacto</a></li>",
                      :disabled => false
 )
