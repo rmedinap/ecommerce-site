42fe1d91391f07df603e005d502de939350635ce
Rafael Medina <rmedinap@gmail.com>
2013-10-02 23:04:56 -0500

    Video en mejor posicion y tamaño. Slides en el carrusel dura ahora 6 segundos

--- config/initializers/spree_videos.rb
+++ config/initializers/spree_videos.rb
@@ -1,17 +1,17 @@
 # Take a look at the Spree::Videos::Configuration class for details
 
-# Spree::Videos.configure do |conf|
-#   conf.youtube_url_params = {
-#     :show_title => "0",
-#     :enablejsapi => "1",
-#     :theme => "light",
-#     :modestbranding => "1",
-#     :wmode => "opaque",
-#     :rel => "0",
-#   }
+Spree::Videos.configure do |conf|
+  conf.youtube_url_params = {
+    :show_title => "0",
+    :enablejsapi => "1",
+    :theme => "light",
+    :modestbranding => "1",
+    :wmode => "opaque",
+    :rel => "0",
+  }
 
-#   conf.html_options = {
-#     :width => 280,
-#     :height => 300
-#   }
-# end
+  conf.html_options = {
+    :width => 340,
+    :height => 200
+  }
+end
