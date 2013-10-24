10db20c0ceb2c2d7ce635cca28ccc877ca72c0b7
Rafael <rmedinap@gmail.com>
2013-10-24 10:54:25 -0500

    añadiendo select2.js a precompile en application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,7 @@ Spreecommerce13c::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
   config.assets.precompile += ['select2.css']
+  config.assets.precompile += ['select2.js']
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
